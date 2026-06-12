3c978ee3ca95f1a930c87a461fbc13779816231b
Girirajan RL <girirajan.r1234@gmail.com>
2026-06-12 10:45:11 +0530

    Update ALLOWED_HOSTS to include all hosts

--- register_form/settings.py
+++ register_form/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-kw&k(j82vew#x+t$nokg+do7l_=ru_l+zk$hg1)6!dfea14e@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
